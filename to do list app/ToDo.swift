663c73726c44ee0ddc61ded9e3db8c33b3a23906
beatriz <bdaristegui@gmail.com>
2020-06-08 22:57:30 -0400

    complete iteration 0

---
+++ to do list app/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  to do list app
+//
+//  Created by Batch  on 6/8/20.
+//  Copyright © 2020 beatriz. All rights reserved.
+//
+
+import UIKit
+ 
+class ToDo {
+    var name = ""
+    var important = false
+}
